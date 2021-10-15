0caffc0c0ed79244a5d6f6dcaba4f3b8e6666b66
John Margaronis <john@johnmargaronis.com>
2021-10-15 15:29:38 -0500

    Added spinner loading to PrivateRoute

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
 
 const PrivateRoute = ({
   component: Component,
@@ -11,14 +12,24 @@ const PrivateRoute = ({
   <Route
     {...rest}
     render={props =>
-      !isAuthenticated && !loading ? (
-        <Redirect to='/login' />
-      ) : (
+      loading ? (
+        <Spinner />
+      ) : isAuthenticated ? (
         <Component {...props} />
+      ) : (
+        <Redirect to='/login' />
       )
     }
   />
 );
+
+// const PrivateRoute = props => {
+//   if (!props.auth.isAuthenticated && !props.auth.loading) {
+//     return <Redirect to='/login' />;
+//   } else {
+//     return <Route exact path={props.path} component={props.component} />;
+//   }
+// };
 
 PrivateRoute.propTypes = {
   auth: PropTypes.object.isRequired
