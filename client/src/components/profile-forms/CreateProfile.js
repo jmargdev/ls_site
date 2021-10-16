70267bc1626db329b615072f4c9a2b9a188fa592
John Margaronis <john@johnmargaronis.com>
2021-10-15 20:09:27 -0500

    Changed 'Go Back' links to redirect to /dashboard

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -211,7 +211,7 @@ const CreateProfile = ({ createProfile, history }) => {
         )}
 
         <input type='submit' className='btn btn-primary my-1' />
-        <Link to='#!' className='btn btn-light my-1'>
+        <Link to='/dashboard' className='btn btn-light my-1'>
           Go Back
         </Link>
       </form>
